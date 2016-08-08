d4d6585f61d399cb23e045c01c0df5df00d030bb
Leonardo Lee <lee.leonardo.king@gmail.com>
2016-08-07 17:08:04 -0700

    Initialized the repo.

---
+++ Cadence/Cadence.h
@@ -0,0 +1,19 @@
+//
+//  Cadence.h
+//  Cadence
+//
+//  Created by Leonardo Lee on 8/7/16.
+//  Copyright © 2016 Leonardo Lee. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Cadence.
+FOUNDATION_EXPORT double CadenceVersionNumber;
+
+//! Project version string for Cadence.
+FOUNDATION_EXPORT const unsigned char CadenceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cadence/PublicHeader.h>
+
+
